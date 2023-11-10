068f0a755b20c6d4457f8bdcd96c0fd3f77d638a
Filipe Valentim <filipevalentim0706@gmail.com>
2023-11-10 14:16:28 -0300

    Tratando erros com exceção personalizada

--- Program.cs
+++ Program.cs
@@ -7,33 +7,40 @@ namespace ProjetoXadrez
   {
     static void Main(string[] args)
     {
-      Tabuleiro tab = new Tabuleiro(8, 8);
-
-      tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-      tab.colocarPeca(new Cavalo(tab, Cor.Preta), new Posicao(0, 1));
-      tab.colocarPeca(new Bispo(tab, Cor.Preta), new Posicao(0, 2));
-      tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 3));
-      tab.colocarPeca(new Dama(tab, Cor.Preta), new Posicao(0, 4));
-      tab.colocarPeca(new Bispo(tab, Cor.Preta), new Posicao(0, 5));
-      tab.colocarPeca(new Cavalo(tab, Cor.Preta), new Posicao(0, 6));
-      tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 7));
-
-      for (int i = 0; i < tab.linhas; i++)
+      try
       {
-        tab.colocarPeca(new Peao(tab, Cor.Preta), new Posicao(1, i));
-        tab.colocarPeca(new Peao(tab, Cor.Branca), new Posicao(6, i));
+        Tabuleiro tab = new Tabuleiro(8, 8);
+
+        tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+        tab.colocarPeca(new Cavalo(tab, Cor.Preta), new Posicao(0, 1));
+        tab.colocarPeca(new Bispo(tab, Cor.Preta), new Posicao(0, 2));
+        tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 3));
+        tab.colocarPeca(new Dama(tab, Cor.Preta), new Posicao(0, 4));
+        tab.colocarPeca(new Bispo(tab, Cor.Preta), new Posicao(0, 5));
+        tab.colocarPeca(new Cavalo(tab, Cor.Preta), new Posicao(0, 6));
+        tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 7));
+
+        for (int i = 0; i < tab.linhas; i++)
+        {
+          tab.colocarPeca(new Peao(tab, Cor.Preta), new Posicao(1, i));
+          tab.colocarPeca(new Peao(tab, Cor.Branca), new Posicao(6, i));
+        }
+
+        tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(7, 0));
+        tab.colocarPeca(new Cavalo(tab, Cor.Branca), new Posicao(7, 1));
+        tab.colocarPeca(new Bispo(tab, Cor.Branca), new Posicao(7, 2));
+        tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(7, 3));
+        tab.colocarPeca(new Dama(tab, Cor.Branca), new Posicao(7, 4));
+        tab.colocarPeca(new Bispo(tab, Cor.Branca), new Posicao(7, 5));
+        tab.colocarPeca(new Cavalo(tab, Cor.Branca), new Posicao(7, 6));
+        tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(7, 7));
+
+        Tela.imprimirTabuleiro(tab);
       }
-
-      tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(7, 0));
-      tab.colocarPeca(new Cavalo(tab, Cor.Branca), new Posicao(7, 1));
-      tab.colocarPeca(new Bispo(tab, Cor.Branca), new Posicao(7, 2));
-      tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(7, 3));
-      tab.colocarPeca(new Dama(tab, Cor.Branca), new Posicao(7, 4));
-      tab.colocarPeca(new Bispo(tab, Cor.Branca), new Posicao(7, 5));
-      tab.colocarPeca(new Cavalo(tab, Cor.Branca), new Posicao(7, 6));
-      tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(7, 7));
-
-      Tela.imprimirTabuleiro(tab);
+      catch (TabuleiroException e)
+      {
+        Console.WriteLine(e.Message);
+      }
     }
   }
 }
