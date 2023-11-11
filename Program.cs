b0bc96259a8f751c0e2fd4be0e311003f66acf2f
Filipe Valentim <filipevalentim0706@gmail.com>
2023-11-11 15:40:28 -0300

    Preparando o programa para testes iniciais

--- Program.cs
+++ Program.cs
@@ -9,37 +9,17 @@ namespace ProjetoXadrez
     {
       try
       {
-        Tabuleiro tab = new Tabuleiro(8, 8);
+        PartidaDeXadrez partida = new PartidaDeXadrez();
 
-        tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-        tab.colocarPeca(new Cavalo(tab, Cor.Preta), new Posicao(0, 1));
-        tab.colocarPeca(new Bispo(tab, Cor.Preta), new Posicao(0, 2));
-        tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 3));
-        tab.colocarPeca(new Dama(tab, Cor.Preta), new Posicao(0, 4));
-        tab.colocarPeca(new Bispo(tab, Cor.Preta), new Posicao(0, 5));
-        tab.colocarPeca(new Cavalo(tab, Cor.Preta), new Posicao(0, 6));
-        tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 7));
-
-        for (int i = 0; i < tab.linhas; i++)
-        {
-          tab.colocarPeca(new Peao(tab, Cor.Preta), new Posicao(1, i));
-          tab.colocarPeca(new Peao(tab, Cor.Branca), new Posicao(6, i));
-        }
-
-        tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(7, 0));
-        tab.colocarPeca(new Cavalo(tab, Cor.Branca), new Posicao(7, 1));
-        tab.colocarPeca(new Bispo(tab, Cor.Branca), new Posicao(7, 2));
-        tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(7, 3));
-        tab.colocarPeca(new Dama(tab, Cor.Branca), new Posicao(7, 4));
-        tab.colocarPeca(new Bispo(tab, Cor.Branca), new Posicao(7, 5));
-        tab.colocarPeca(new Cavalo(tab, Cor.Branca), new Posicao(7, 6));
-        tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(7, 7));
-
-        Tela.imprimirTabuleiro(tab);
+        Tela.imprimirTabuleiro(partida.tab);
       }
       catch (TabuleiroException e)
       {
         Console.WriteLine(e.Message);
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine("ERROR: " + e.Message);
       }
     }
   }
